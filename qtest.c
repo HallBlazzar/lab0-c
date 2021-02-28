8daef9f234c4c29d21eac1363b35b33c317343b0
hallblazzar <hallblazzar@gmail.com>
2021-03-01 03:50:21 +0800

    Fix still reachable issue for command from file
    1. qtest.c/main()
      - Confirm that initlizing linenoise(by linenoiseHistoryLoad())
        under command file reading mode(-f option) could cause
        still-reachable memory issue while exiting. The reason is the
        history pointer(in linenoise.c) is left unused and unreleased
        under the mode. To prevent the issue, add condition to prevent
        initializing linenoise under the mode.

--- qtest.c
+++ qtest.c
@@ -762,14 +762,17 @@ int main(int argc, char *argv[])
     init_cmd();
     console_init();
 
-    /* Trigger call back function(auto completion) */
-    linenoiseSetCompletionCallback(completion);
+    if (!infile_name) {
+        /* Trigger call back function(auto completion) */
+        linenoiseSetCompletionCallback(completion);
 
-    linenoiseHistorySetMaxLen(HISTORY_LEN);
-    linenoiseHistoryLoad(HISTORY_FILE); /* Load the history at startup */
+        linenoiseHistorySetMaxLen(HISTORY_LEN);
+        linenoiseHistoryLoad(HISTORY_FILE); /* Load the history at startup */
+    }
+
     set_verblevel(level);
     if (level > 1) {
-        set_echo(1);
+        set_echo(true);
     }
     if (logfile_name)
         set_logfile(logfile_name);
